c6503bc380a436ad1a53d7ca1e5abc878c212d9c
r0m0enc <rodrigo.martinez5@walmart.com>
2025-11-12 20:08:16 -0300

    add remaps, eslint and diagnostics true

--- lua/rodri/lazy/lsp.lua
+++ lua/rodri/lazy/lsp.lua
@@ -6,17 +6,27 @@ return {
             "williamboman/mason-lspconfig.nvim",
         },
         config = function()
-            --vim.lsp.config("lua_ls", {})
-            --vim.lsp.enable("lua_ls")
             require("mason").setup()
             require("mason-lspconfig").setup({
                 ensure_installed = {
                     "lua_ls",
                     "ts_ls",
+                    "eslint",
                 },
             })
-            --vim.keymap.set("n", "<leader>rr", vim.lsp.buf.references())
-            vim.diagnostic.config({virtual_text = true})
+            local diagnosticConfig = {
+                virtual_text = true,
+            }
+            vim.diagnostic.config(diagnosticConfig)
+            vim.api.nvim_create_autocmd("LspAttach", {
+                callback = function(ev)
+                    local bufnr = ev.buf
+                    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+                    vim.keymap.set("n", "<leader>rr", vim.lsp.buf.references)
+                    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
+                    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
+                end
+            })
         end,
     }
 }
